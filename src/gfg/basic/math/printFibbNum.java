ad9872734efd7ce21e01d29df66e16af325b8f5e
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-24 18:31:49 +0530

    [GFG][JAVA][Math] print nth fib number example solution

---
+++ src/gfg/basic/math/printFibbNum.java
@@ -0,0 +1,24 @@
+package gfg.basic.math;
+
+import java.util.Arrays;
+
+public class printFibbNum {
+
+    static long[] printFibbNum(int n) {
+        long fn = 1;
+        long sn = 1;
+
+        long[] result = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            result[i] = fn;
+            long temp = fn;
+            fn = sn;
+            sn += temp;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(printFibbNum(5)));
+    }
+}
