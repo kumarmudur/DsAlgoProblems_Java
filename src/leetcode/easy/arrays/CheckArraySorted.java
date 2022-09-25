18e855e65464afbb64af28a1c1242da45567d369
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-25 12:58:41 +0530

    [Leetcode][JAVA][Array][Sorting] check is array sorting example solution 2

--- src/leetcode/easy/arrays/CheckArraySorted.java
+++ src/leetcode/easy/arrays/CheckArraySorted.java
@@ -13,8 +13,18 @@ public class CheckArraySorted {
         }
         return true;
     }
+
+    // time: O(N) | space: O(1)
+    static boolean checkIsSortedArray1(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[i - 1]) return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 3,4,5,1,2 };
-        System.out.println(checkIsSortedArray(arr));
+        System.out.println(checkIsSortedArray1(arr));
     }
 }
