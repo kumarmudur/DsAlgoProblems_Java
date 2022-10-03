329e6cb9d8027227d573def694d4d95284b76ee2
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-10-03 08:09:06 +0530

    [Leetcode][JAVA][4][Hard][Arrays][Binary Search][D&C] find median of sorted arrays example solution

---
+++ src/leetcode/hard/FindMedianSortedArrays.java
@@ -0,0 +1,51 @@
+package leetcode.hard;
+
+// Topic: Arrays, Binary Search, Divide and Conquer
+
+public class FindMedianSortedArrays {
+
+    // Solution 1
+    static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[] a = new int[m + n];
+        int i = 0, j = 0, k = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                a[k] = nums1[i];
+                i++;
+            } else {
+                a[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < m) {
+            a[k] = nums1[i];
+            i++;
+            k++;
+        }
+
+        while (j < n) {
+            a[k] = nums2[j];
+            j++;
+            k++;
+        }
+        if ((m + n) % 2 != 0) {
+            return a[(m + n) / 2];
+        } else {
+            int x = (m + n) / 2;
+            double p = a[x];
+            double q = a[x - 1];
+            return (p + q) / 2;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = { 1, 2 };
+        int[] nums2 = { 3, 4 };
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
+}
