f9d8c7a8a0df5598cef3fc55005cd33f19739d35
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-10-25 11:17:48 +0530

    [Leetcode][JAVA][Easy][Arrays] hammingWeight example solution

---
+++ src/leetcode/easy/arrays/HammingWeight.java
@@ -0,0 +1,13 @@
+package leetcode.easy.arrays;
+
+public class HammingWeight {
+
+    public int hammingWeight(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum++;
+            n &= (n - 1);
+        }
+        return sum;
+    }
+}
