21c3f795c49261cf446bc8d955e660f9cae7ad47
kumarmudur <kumarmudur10@gmail.com>
2023-01-24 09:31:29 +0530

    [Leetcode][Easy][Array] meeting room solution 1

---
+++ src/leetcode/easy/arrays/MeetingRoom.java
@@ -0,0 +1,20 @@
+package leetcode.easy.arrays;
+
+public class MeetingRoom {
+
+    public boolean canAttendMeetings(int[][] intervals) {
+        for (int i = 0; i < intervals.length; i++) {
+            for (int j = i + 1; j < intervals.length; j++) {
+                if (overlap(intervals[i], intervals[j])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean overlap(int[] interval1, int[] interval2) {
+        return (interval1[0] >= interval2[0] && interval1[0] < interval2[1])
+                || (interval2[0] >= interval1[0] && interval2[0] < interval1[1]);
+    }
+}
