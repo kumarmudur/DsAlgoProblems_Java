982c802013de31db0e51018aa450107418c14a16
kumarmudur <kumarmudur10@gmail.com>
2023-01-26 17:47:41 +0530

    [Leetcode][Easy][Array] meeting room solution 2 example

--- src/leetcode/easy/arrays/MeetingRoom.java
+++ src/leetcode/easy/arrays/MeetingRoom.java
@@ -1,5 +1,7 @@
 package leetcode.easy.arrays;
 
+import java.util.Arrays;
+
 // Solution 1
 // time: O(n^2) | space O(1)
 public class MeetingRoom {
@@ -18,5 +20,16 @@ public class MeetingRoom {
     public boolean overlap(int[] interval1, int[] interval2) {
         return (interval1[0] >= interval2[0] && interval1[0] < interval2[1])
                 || (interval2[0] >= interval1[0] && interval2[0] < interval1[1]);
+    }
+
+    // Solution 2
+    public boolean canAttendMeetings1(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i][1] > intervals[i + 1][0]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
