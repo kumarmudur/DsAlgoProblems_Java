ecc8f729a75ee37b4e63598e3d6f34521ea4c830
Basavakumar Mudur <kumarmudur10@gmail.com>
2023-02-07 07:08:38 +0530

    [Leetcode][JAVA][Easy][String][Math] excel sheet column number example solution

--- src/leetcode/easy/math/ExcelSheetColumnNumber.java
+++ src/leetcode/easy/math/ExcelSheetColumnNumber.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class ExcelSheetColumnNumber {
 
+    // Solution 1
     public static int titleToNumber(String columnTitle) {
         int result = 0;
         Map<Character, Integer> alpha_map = new HashMap<>();
@@ -18,6 +19,17 @@ public class ExcelSheetColumnNumber {
         for (int i = 0; i < n; i++) {
             char cur_char = columnTitle.charAt(n - 1 - i);
             result += (alpha_map.get(cur_char) * (Math.pow(26, i)));
+        }
+        return result;
+    }
+
+    // Solution 2
+    public static int titleNumber1(String columnTitle) {
+        int result = 0;
+        int n = columnTitle.length();
+        for (int i = 0; i < n; i++) {
+            result = result * 26;
+            result += (columnTitle.charAt(i) - 'A' + 1);
         }
         return result;
     }
