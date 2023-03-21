c82aaadc810b6b8d7c2518bc4829a924fd68b138
kumarmudur <kumarmudur10@gmail.com>
2023-03-21 21:59:08 +0530

    [Leetcode][Medium][Array] combination sum example

--- src/leetcode/dataStructure/easy/ContainsDuplicate.java
+++ src/leetcode/dataStructure/easy/ContainsDuplicate.java
@@ -6,12 +6,17 @@ public class ContainsDuplicate {
 
     // Solution 1
     // time: O(n^2) | space: O(1)
-    public boolean containsDuplicate(int[] nums) {
+    public static boolean containsDuplicate(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] == nums[j]) return true;
             }
         }
         return false;
+    }
+
+
+    public static void main(String[] args) {
+        containsDuplicate(new int[]{1, 2, 3, 4});
     }
 }
